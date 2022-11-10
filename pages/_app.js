6bd201948e83f05498ddd573e2358d5675d85188
Tae Lee <taekyu.lee3@gmail.com>
2022-11-11 02:11:49 +1100

    title tab

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>FTX Classic</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
